50438d1939307e2481b7899dc889cad2eada15de
leehwan93 <48823337+leehwan93@users.noreply.github.com>
2019-03-22 19:38:05 +1100

    Module 1 Task 2

---
+++ Module1/Task1.2P.cpp
@@ -0,0 +1,35 @@
+int movingState = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(2, INPUT);
+  pinMode(13, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(2),pin_ISR,CHANGE);
+}
+
+void loop()
+{
+  //Serial.println(digitalRead(2));
+
+  if (movingState == HIGH) 
+  {
+    digitalWrite(13, HIGH);
+  } else 
+  {
+    digitalWrite(13, LOW);
+  }
+  
+}
+
+void pin_ISR()
+{
+  movingState = movingState==0? 1: 0;
+  
+  switch(movingState){
+    case 0: Serial.println("moving object gone");
+    break;
+    case 1: Serial.println("moving object detected");
+    break;
+  }
+}
